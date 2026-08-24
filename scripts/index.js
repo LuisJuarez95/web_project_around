d564423b76424c76dc058a9eb7d9aa1e12a22793
LuisJuarez95 <luisjuarezmj9.5@gmail.com>
2026-08-23 21:28:43 -0600

    Genera las tarjetas iniciales desde JS usando un template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "./images/yosemite-valley.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "./images/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "./images/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "./images/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "./images/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "./images/lago-di-braies.jpg",
+  },
+];
+
 const popupEditProfile = document.querySelector(".popup_type_edit");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = popupEditProfile.querySelector(".popup__close");
@@ -10,7 +37,8 @@ const saveButton = editFormElement.querySelector(".popup__save-button");
 const profileNameElement = document.querySelector(".profile__name");
 const profileAboutElement = document.querySelector(".profile__about");
 
-const likeButtons = document.querySelectorAll(".card__like-button");
+const cardTemplate = document.querySelector(".card-template").content;
+const cardsList = document.querySelector(".cards__list");
 
 function openPopup(popupElement) {
   popupElement.classList.add("popup_opened");
@@ -45,10 +73,30 @@ function handleProfileFormSubmit(evt) {
   closePopup(popupEditProfile);
 }
 
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardTitleElement.textContent = cardData.name;
+
+  cardLikeButton.addEventListener("click", handleLikeButtonClick);
+
+  return cardElement;
+}
+
+function renderCard(cardData) {
+  const cardElement = createCard(cardData);
+  cardsList.append(cardElement);
+}
+
 editButton.addEventListener("click", handleEditProfileClick);
 closeButton.addEventListener("click", () => closePopup(popupEditProfile));
 editFormElement.addEventListener("submit", handleProfileFormSubmit);
 nameInput.addEventListener("input", updateSaveButtonState);
 aboutInput.addEventListener("input", updateSaveButtonState);
 
-likeButtons.forEach((button) => button.addEventListener("click", handleLikeButtonClick));
+initialCards.forEach(renderCard);
